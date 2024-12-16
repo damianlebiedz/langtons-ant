f61e7acfecf0cbd54fa56833fcdb5f161b9718e8
damianlebiedz <damian.lebiedz@gmail.com>
2024-12-16 15:17:59 +0100

    commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Mrówka Langtona | W TRAKCIE TWORZENIA
+
+Symulacja mrówki Langtona w C++
+
+## Spis treści
+- [Technologie](#technologie)
+- [Instalacja](#instalacja)
+- [Screenshoty](#screenshoty)
+- [Kontakt](#kontakt)
+
+## Technologie
+- C++
+- SFML
+- CMake
+
+## Instalacja
+
+## Screenshoty
+
+## Kontakt
+Damian Lebiedź | 
+https://damianlebiedz.github.io
---
+++ src/Renderer.cpp
@@ -0,0 +1,19 @@
+#include "Renderer.h"
+
+Renderer::Renderer(int gridSize) : gridSize(gridSize), cellSize(600.0f / gridSize) {}
+
+void Renderer::render(Grid& grid, Ant& ant, sf::RenderWindow& window) {
+    sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
+
+    for (int y = 0; y < gridSize; ++y) {
+        for (int x = 0; x < gridSize; ++x) {
+            cell.setPosition(x * cellSize, y * cellSize);
+            cell.setFillColor(grid.getCell(x, y) == 0 ? sf::Color::White : sf::Color::Black);
+            window.draw(cell);
+        }
+    }
+    sf::RectangleShape antShape(sf::Vector2f(cellSize, cellSize));
+    antShape.setPosition(ant.getX() * cellSize, ant.getY() * cellSize);
+    antShape.setFillColor(sf::Color::Red);
+    window.draw(antShape);
+}
